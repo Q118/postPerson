df33a58ab67d5b6e3ad3ca1369b80430af1ff836
Shelby Rothman <srothman@corelation.local>
2021-08-19 16:29:48 -0700

    finished html hardcoded

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+
+// section where we are going to add in the ke value pairs
+
+const queryParamsContainer = document.querySelector('[data-query-params]')
+
+const requestHeadersContainer = document.querySelector('[data-request-headers]')
+
+const keyValueTemplate = document.querySelector('[data-key-value-template]')
+
+queryParamsContainer.append(createKeyValuePair())
+requestHeadersContainer.append(createKeyValuePair())
+
+function createKeyValuePair() {
+    const element = keyValueTemplate.textContent.cloneNode(true)
+}
